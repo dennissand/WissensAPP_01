370f8a2e201921a910b28ff56c38ffacc9e6cde3
Dennis Sand <97163222+dennissand@users.noreply.github.com>
2022-06-26 01:13:00 +0200

    Design hinzugefügt Alle Funktionen geschrieben

--- app/src/main/java/com/example/wissensapp_01/MainActivity.kt
+++ app/src/main/java/com/example/wissensapp_01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wissensapp_01
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,16 +9,29 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.wissensapp_01.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    // Einbinden von Firebase Authentication//
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Firebase wird abgerufen //
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // LogOut Funktion wenn man die Verlassen möchte //
+        binding.logOut.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            firebaseAuth.signOut()
+            startActivity(intent)
+        }
 
         val navView: BottomNavigationView = binding.navView
 
